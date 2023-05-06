68c2882c6cd2878643fe5ca710f27f34b824574b
KRVitalik <karmelitavitalik@gmail.com>
2023-05-06 13:58:08 +0300

    video-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const saveTimeVideo = localStorage.getItem("videoplayer-current-time")
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(saveTimeVideo).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
