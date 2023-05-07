964bca292f95db5dc95abbbad0166ca9e2ec5ff0
KRVitalik <karmelitavitalik@gmail.com>
2023-05-07 19:27:28 +0300

    feedback-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,42 @@
-const onForm = document.querySelector('form')
+import throttle from 'lodash.throttle';
 
+const input = document.querySelector('input')
+const textarea = document.querySelector('textarea')
+const form = document.querySelector('form')
 
-let form = {
-    email: onForm.email.value,
-    message: onForm.message.value,
+form.addEventListener('input', throttle(onInput, 500))
+form.addEventListener('submit', onFormSubmit)
+
+const storageKey = "feedback-form-state";
+
+let userForm = {};
+
+onLocalStorageForm()
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    if (input.value === '' || textarea.value === '') {
+        return
+    }
+    
+    console.log(userForm);
+    localStorage.removeItem(storageKey)
+    e.currentTarget.reset();
 }
-console.log(form);
 
-onForm.addEventListener('form', () => {
-})
-// console.log(onForm.email);
-// let saveOnForm = localStorage.setItem("feedback-form-state", JSON.stringify(form));
-// console.log(onForm.message);
+function onInput() {
+    userForm = {
+        email: input.value,
+        message: textarea.value,
+    }
+    localStorage.setItem(storageKey, JSON.stringify(userForm))
+}
+
+function onLocalStorageForm() {
+    const saveForm = JSON.parse(localStorage.getItem(storageKey))
+    
+    if (saveForm) {
+        input.value = saveForm.email
+        textarea.value = saveForm.message
+    }
+}
