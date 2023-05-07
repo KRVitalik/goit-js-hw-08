a09003746cdd6e39eb4e85075c2ad9126a3b1f8e
KRVitalik <99497132+KRVitalik@users.noreply.github.com>
2023-05-07 08:13:05 +0000

    Deploying to gh-pages from @ KRVitalik/goit-js-hw-08@ba354eefcb09f35ada5e2833dce514284492890e 🚀

---
+++ 03-feedback.1cb903ff.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("form"),o={email:e.email.value,message:e.message.value};console.log(o),e.addEventListener("form",(function(){}))}();
+//# sourceMappingURL=03-feedback.1cb903ff.js.map
