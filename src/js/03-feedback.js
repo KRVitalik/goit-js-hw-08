18ee708cc62f3345464d4dec06e784ba43c13da0
KRVitalik <karmelitavitalik@gmail.com>
2023-05-07 10:45:30 +0300

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+const onForm = document.querySelector('form')
 
+
+let form = {
+    email: onForm.email.value,
+    message: onForm.message.value,
+}
+console.log(form);
+
+onForm.addEventListener('form', () => {
+})
+// console.log(onForm.email);
+// let saveOnForm = localStorage.setItem("feedback-form-state", JSON.stringify(form));
+// console.log(onForm.message);
